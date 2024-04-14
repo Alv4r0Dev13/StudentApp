1e337f9a9096e0076d31ea28e730789764b0726d
Álvaro Renan <alvarorenancf00@gmail.com>
2024-04-14 16:49:38 -0300

    Creating 'Delete student' functionality

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,8 +1,14 @@
 import { get } from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { FaEdit, FaUserCircle, FaWindowClose } from 'react-icons/fa';
+import {
+  FaEdit,
+  FaExclamation,
+  FaUserCircle,
+  FaWindowClose,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
+import { toast } from 'react-toastify';
 import Loading from '../../components/Loading';
 import secret from '../../secret';
 import axios from '../../services/axios';
@@ -23,12 +29,37 @@ export default function Students() {
     getData();
   }, []);
 
+  const handleDeleteAsk = (evt) => {
+    evt.preventDefault();
+    const exclamation = evt.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    evt.currentTarget.remove();
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const handleDelete = async (evt, id, index) => {
+    evt.persist();
+    try {
+      setIsLoading(true);
+      await axios.delete(`/students/${id}`);
+      const newStudents = [...students];
+      newStudents.splice(index, 1);
+      setStudents(newStudents);
+      setIsLoading(false);
+    } catch (e) {
+      const status = get(e, 'response.status', 0);
+      if (status === 401) toast.error('Login required!');
+      else toast.error('Error while deleting student!');
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Students</h1>
       <StudentContainer>
-        {students.map((student) => (
+        {students.map((student, index) => (
           <div key={String(student.id)}>
             <ProfilePicture>
               {get(student, 'Photos[0].url', false) ? (
@@ -42,13 +73,23 @@ export default function Students() {
               )}
             </ProfilePicture>
             <p>{student.name}</p>
+            <p>{student.lastname}</p>
             <p>{student.email}</p>
             <Link to={`/student/${student.id}/edit`}>
               <FaEdit size={16} />
             </Link>
-            <Link to={`/student/${student.id}/delete`}>
+            <Link
+              to={`/student/${student.id}/delete`}
+              onClick={handleDeleteAsk}
+            >
               <FaWindowClose size={16} />
             </Link>
+            <FaExclamation
+              size={16}
+              display="none"
+              cursor="pointer"
+              onClick={(e) => handleDelete(e, student.id, index)}
+            />
           </div>
         ))}
       </StudentContainer>
