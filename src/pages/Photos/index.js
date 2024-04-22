24da57dd18918f99702a77bb61ecaca20c853eb0
Álvaro Renan <alvarorenancf00@gmail.com>
2024-04-22 09:13:30 -0300

    Creating 'Edit photo' functionality

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 import { Container } from '../../styles/Global';
+import { Form } from './styled';
 
-export default function Photos() {
+export default function Photos({ match }) {
+  const dispatch = useDispatch();
+  const id = get(match, 'params.id', '');
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/students/${id}`);
+        setPhoto(get(data, 'Photos[0].url', ''));
+        setIsLoading(false);
+      } catch {
+        toast.error('Error while obtaining image!');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+    getData();
+  }, [id]);
+
+  const handleSubmit = async (evt) => {
+    const file = evt.target.files[0];
+    const fileURL = URL.createObjectURL(file);
+    setPhoto(fileURL);
+
+    const formData = new FormData();
+    formData.append('student_id', id);
+    formData.append('file', file);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/photos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Sent photo successfully.');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      // eslint-disable-next-line no-console
+      console.log(err);
+      const status = get(err, 'response.status', 0);
+      toast.error('Error while sending photo!');
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Photos</h1>
+      <Form>
+        <label htmlFor="photo">
+          {photo ? <img src={photo} alt="Profile" /> : 'Select photo'}
+          <input type="file" name="photo" id="photo" onChange={handleSubmit} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Photos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Photos/styled.js
+++ src/pages/Photos/styled.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
+import { primary } from '../../config/colors';
 
-export const Title = styled.h1`
-  color: red;
+export const Form = styled.form`
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    background-color: #eee;
+    border: 5px dashed ${primary};
+    margin: 30px auto;
+    cursor: pointer;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+
+    img {
+      width: 180px;
+      height: 180px;
+    }
+  }
+
+  input {
+    display: none;
+  }
 `;
